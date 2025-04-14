856d819ecbcf3d0679fd2bba163dd9016aa1e160
aayushker <singhaayushker@gmail.com>
2025-04-14 22:55:09 +0530

    feat: add ComparisonPage component with ProductComparison functionality

---
+++ frontend/src/pages/ComparisonPage.jsx
@@ -0,0 +1,11 @@
+import ProductComparison from '../components/ui/ProductComparison';
+
+function ComparisonPage() {
+  return (
+    <div className="pt-20">
+      <ProductComparison />
+    </div>
+  );
+}
+
+export default ComparisonPage; 
