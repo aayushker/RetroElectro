4ee783074ebefe86fbea2aa65784f1d7eb6b8abe
aayushker <singhaayushker@gmail.com>
2025-05-16 23:44:58 +0530

    feat: add recommendation routes for handling recommendations and query logging

---
+++ server/routes/recommendationRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const { 
+  getRecommendations,
+  logQuery
+} = require('../controllers/recommendationController');
+
+router.post('/recommend', getRecommendations);
+router.post('/query-log', logQuery);
+
+module.exports = router; 
